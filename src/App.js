725e08888ecd0461c12097d3ab87728cd1bc7c00
b2win <68144560+b2win@users.noreply.github.com>
2021-06-02 17:06:14 +0900

    게시판 페이지네이션 구현중
    게시판 페이지네이션 구현중

--- src/App.js
+++ src/App.js
@@ -1,27 +1,75 @@
-import Board from "./Board";
-import Categories from "./Categories";
-import Corona from "./Corona";
-import BusinessBoard from "./BusinessBoard";
-import EntertainmentBoard from "./EntertainmentBoard";
-import NewsBulletin from "./NewsBulletin";
-import { Route } from "react-router-dom";
+// import Board from "./Board";
+// import Categories from "./Categories";
+// import Corona from "./Corona";
+// import BusinessBoard from "./BusinessBoard";
+// import EntertainmentBoard from "./EntertainmentBoard";
+// import NewsBulletin from "./NewsBulletin";
+// import { Route } from "react-router-dom";
 
-// const express = require("express");
-// const cors = require("cors");
-// const app = express();
+// // const express = require("express");
+// // const cors = require("cors");
+// // const app = express();
 
-// app.use(cors());
+// // app.use(cors());
+
+// function App() {
+//   return (
+//     <>
+//       {/* <Categories /> */}
+//       {/* <Board /> */}
+//       {/* <BusinessBoard /> */}
+//       {/* <EntertainmentBoard /> */}
+//       {/* <Corona /> */}
+//       <Route path="/:category?" component={NewsBulletin} />
+//     </>
+//   );
+// }
+
+// export default App;
+
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Posts from "./Post";
+import Pagination from "./Pagination";
 
 function App() {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage, setPostsPerPage] = useState(10);
+
+  useEffect(() => {
+    async function fetchPosts() {
+      setLoading(true);
+      const response = await axios.get(
+        "https://jsonplaceholder.typicode.com/posts"
+      );
+      setPosts(response.data);
+      setLoading(false);
+    }
+    fetchPosts();
+  }, []);
+
+  console.log(posts);
+
+  const indexOfLast = currentPage * postsPerPage;
+  const indexOfFirst = indexOfLast - postsPerPage;
+  function currentPosts(tmp) {
+    let currentPosts = 0;
+    currentPosts = tmp.slice(indexOfFirst, indexOfLast);
+    return currentPosts;
+  }
+
   return (
-    <>
-      {/* <Categories /> */}
-      {/* <Board /> */}
-      {/* <BusinessBoard /> */}
-      {/* <EntertainmentBoard /> */}
-      {/* <Corona /> */}
-      <Route path="/:category?" component={NewsBulletin} />
-    </>
+    <div className="App">
+      <Posts posts={currentPosts(posts)} loading={loading}></Posts>
+      <Pagination
+        postsPerPage={postsPerPage}
+        totalPosts={posts.length}
+        paginate={setCurrentPage}
+      ></Pagination>
+    </div>
   );
 }
 
